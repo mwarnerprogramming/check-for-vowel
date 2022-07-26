89cf667d2da33d567a4f115c0f6189fd251b381b
mwarnerprogramming <109999168+mwarnerprogramming@users.noreply.github.com>
2022-07-25 21:31:53 -0400

    update to main
    Changed the user input to a char and performed char-to-string conversion.

--- main.cpp
+++ main.cpp
@@ -18,13 +18,16 @@ using std::regex_match;
 
 int main() {
 
-    string userInput;
+    char userInput;
     regex checkForChars("[aeiouAEIOU]");
 
     cout << "Enter a character to see if it's a vowel or a consenant:\n";
     cin >> userInput;
 
-    if( regex_match ( userInput, checkForChars ) ) { cout << "\nCharacter is a vowel."; }
+    string check;
+    check.append(1, userInput);
+
+    if( regex_match ( check, checkForChars ) ) { cout << "\nCharacter is a vowel."; }
     else { cout << "\nCharacter is a consenant."; }
 
     return 0;
